40852df2e22ab7535c2ea47645e4619a98f60bee
Priyal Parikh <parikhpriyal91@yahoo.com>
2020-08-14 18:29:31 +0530

    Refactor: Introduced the logic in a separate method for code conversion

--- src/main/java/com/bnpp/kata/TennisGame.java
+++ src/main/java/com/bnpp/kata/TennisGame.java
@@ -5,15 +5,23 @@ public class TennisGame
 
     private final String playerOne;
     private final String playerTwo;
+    private int playerOneScore;
+
 
     public TennisGame(String playerOneName, String playerTwoName) {
         this.playerOne=playerOneName;
         this.playerTwo=playerTwoName;
     }
 
-    public String getScore() {
-        return "Love All";
+    public String getScore()
+    {
+        return convertToReadableScore(0)+" All";
     }
+
+    private String convertToReadableScore(int numericScore) {
+        return "Love";
+    }
+
 
     /* getters */
 
